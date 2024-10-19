ee213354d1363cf2448baadb7322c33312d63d01
Thearyan3 <160833326+Thearyan3@users.noreply.github.com>
2024-10-19 20:10:52 +0530

    Add files via upload

---
+++ A2.js
@@ -0,0 +1,110 @@
+// // Question No. 1 - Find Grades based upon the marks(M) obtained by a student. 
+// function findGradesFunction(){
+//     var M = 15;
+//     switch(true){
+//         case(41<= M && M<= 50):
+//                 console.log("A");
+//                 break;
+//         case(31<= M && M<= 40):
+//                 console.log("B");
+//                 break;
+//         case(21<= M && M<= 30):
+//                 console.log("C");
+//                 break;
+//         case(11<= M && M<= 20):
+//                 console.log("D");
+//                 break;
+//         case(M <= 10):
+//                 console.log("E");
+//                 break;
+//         default:
+//                 console.log("Invalid Marks");
+//                 console.log("Enter the Marks from 0 to 50");
+//                 break;
+//     }
+// }
+// findGradesFunction();
+
+// Question No. 2 - Find the value from tthe table corresponding to an input character and return it. 
+// function getValue(){
+// let C = "P";
+// if(C === "P" || C === "p"){
+//         console.log("PrepBytes");
+// }
+// else if(C === "Z" || C === "z"){
+//         console.log("Zenith");
+// }
+// else if(C === "E" || C === "e"){
+//         console.log("Expert Coder");
+// }
+// else if(C === "D" || C === "d"){
+//         console.log("data Structure");
+// }
+// else{
+//         console.log("Invalid");
+// }
+// }
+// getValue();
+
+// Question No. 3 - Take three numbers and print the largest among them. 
+// function Max_out_of_three(){
+// let A = 2;
+// let B = 2;
+// let C = 2;
+// if(A > B){
+//         if(A > C){
+//                 console.log(A);
+//         }else{
+//                 console.log(C);
+//         }
+// }else{
+//         if(B > C){
+//                 console.log(B);
+//         }else{
+//                 console.log(C);
+//         }
+// }
+// if(A == B && B == C){
+//         console.log("-1");
+// }else{
+//         console.log("Not Equal");
+// }
+// // }
+// // Max_out_of_three();
+
+// Question No. 4 - find and return second smallest integer among the three integers. 
+// function findSecondSmallest(){
+// let X = 9;
+// let Y = 23;
+// let Z = 2;
+// if(X > Y){
+//         if(X > Z){
+//                 console.log(Z);
+//         }else{
+//                 console.log(X);
+//         }
+// }else{
+//         if(Y > Z){
+//                 console.log(Z);
+//         }else{
+//                 console.log(Y);
+//         }
+// }
+// }
+// findSecondSmallest();
+
+// Question No. 5 - Check Whether the triangle is Acute or Obtuse 
+// function Triangle_Check(){
+//         let A = 60;
+//         let B = 1;
+//         let C = 20;
+//         let Sum = (A + B + C) ;
+//         if(Sum < 90){
+//                 console.log("Acute");
+//         }else if(Sum = 90){
+//                 console.log("Right Angle Triangle");
+//         }else{
+//                 console.log("Obtuse");
+//         }
+// }
+// Triangle_Check();
